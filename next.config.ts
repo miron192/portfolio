a7060e34c5e5df0dbf7df4c6774d0a8d4cd872b3
Miron <67469916+miron192@users.noreply.github.com>
2025-10-02 12:29:25 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,25 @@
-import type { NextConfig } from 'next'; // Importă tipul NextConfig
-import type { WebpackConfig } from 'next/dist/server/config-shared'; // Importă tipul pentru Webpack
-
-const nextConfig: NextConfig = {
+const nextConfig = {
+  experimental: {
+    turbopack: true, // Activează Turbopack
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        pathname: '/lrigu76hy/**',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/lrigu76hy/**",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'skillicons.dev',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "skillicons.dev",
+        pathname: "/**",
       },
     ],
-  },
-  webpack(config: WebpackConfig) { // Specifică tipul corect pentru config
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'], // Utilizează SVGR pentru a transforma SVG-uri în componente React
-    });
-    return config;
   },
 };
 
